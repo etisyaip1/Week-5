57548a8e4b6448e7dbd3bdaba58ad3d197310e4d
Etisyai <etisyaip@gmail.com>
2024-10-23 15:37:29 -0400

    wip

--- game/game.js
+++ game/game.js
@@ -1,9 +1,3 @@
-
-// const things = []
-// const entitites = []
-
-const spriteList = [] //things that can move on screen
-
 function Position(px, py) {
     return { x: px, y: py };
 }
@@ -17,32 +11,47 @@ function Sprite(nameParameter, kindParameter, posParameter) {
     };
 }
 
-spriteList.push(Sprite("Capt.Lee", "wizard", Position(10, 5)));
-spriteList.push(Sprite("Capt.Jenny", "wizard", Position(10, 5)));
-spriteList.push(Sprite("Venom", "parasite", Position(20, 10)));
+// const things = []
+// const entities = []
+
+const spriteList = []; // array of things that can move on screen
+
+spriteList.push(Sprite("Capt. Lee", "wizard", Position(10, 5)));
+spriteList.push(Sprite("Capt. Jenny", "wizard", Position(5, 15)));
+spriteList.push(Sprite("Venom1", "parasite", Position(20, 10)));
 spriteList.push(Sprite("Venom2", "parasite", Position(15, 10)));
 spriteList.push(Sprite("Venom3", "parasite", Position(20, 15)));
 
 function printList(list){
-    for (let i = 0; i < list.length; i++){
-        console.log(i, list[i])
+    for (let i = 0; i < list.length; i++) {
+        console.log(i, list[i]);
     }
 }
 
 function getHeroList(){
-    const list = []; // array
-    for(let i = 0; i < spriteList.length; i++) {
-        if(spriteList[i].kind === "wizard"){
-            outputList.push(spriteList[i]); //add to array list
-    }
-}
-return outputList; //send array back
-}
-function getParasites(){
+    const outputList = []; // array 
     for (let i = 0; i < spriteList.length; i++) {
-        if(spriteList[i].kind == "parasite"){
-            console.log(i, spriteList[i]. kind);
+        if(spriteList[i].kind === "wizard"){ // filter hero wizards only
+            outputList.push(spriteList[i]); //add to array list
         }
     }
+    return outputList; //send array back
 }
-printParasites()
+
+printList(getHeroList());
+
+// function printHero(){
+//     for (let i = 0; i < spriteList.length; i++) {
+//         if(spriteList[i].kind === "wizard"){
+//             console.log(i, spriteList[i]);
+//         }
+//     }
+// }
+
+// function printParasites(){
+//     for (let i = 0; i < spriteList.length; i++) {
+//         if(spriteList[i].kind === "parasite"){
+//             console.log(i, spriteList[i]);
+//         }
+//     }
+// }
